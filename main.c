1beb217d496441ae2c71fe4e395c87853ecb3420
ummanoj01-cmd <ummanoj01@gmail.com>
2026-09-15 11:05:38 +0530

    Manoj UM-Add line editor core features

--- main.c
+++ main.c
@@ -140,3 +140,62 @@ void deleteLine(int position)
 
     printf("Line deleted successfully.\n");
 }
+void displayLines()
+{
+    int i;
+
+    if (lineCount == 0)
+    {
+        printf("Document is empty.\n");
+        return;
+    }
+
+    printf("\n----- DOCUMENT -----\n");
+
+    for (i = 0; i < lineCount; i++)
+    {
+        printf("%d. %s\n", i + 1, lines[i]);
+    }
+
+    printf("--------------------\n");
+}
+
+void searchLines(char word[])
+{
+    int i;
+    int found = 0;
+
+    for (i = 0; i < lineCount; i++)
+    {
+        if (strstr(lines[i], word) != NULL)
+        {
+            printf("Found \"%s\" at line %d\n", word, i + 1);
+            found = 1;
+        }
+    }
+
+    if (found == 0)
+    {
+        printf("\"%s\" not found.\n", word);
+    }
+}
+
+void countWords()
+{
+    int i, j, words = 0;
+
+    for (i = 0; i < lineCount; i++)
+    {
+        for (j = 0; lines[i][j] != '\0'; j++)
+        {
+            if (lines[i][j] != ' ' &&
+                (j == 0 || lines[i][j - 1] == ' '))
+            {
+                words++;
+            }
+        }
+    }
+
+    printf("Lines: %d\n", lineCount);
+    printf("Words: %d\n", words);
+}
